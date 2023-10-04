a7118fe733fe904c45c9e8c1c954bcd1c8d28ff8
Yuchen Liu <101923999+alexliu07@users.noreply.github.com>
2023-10-04 12:08:02 +0800

    Changed

--- test/test.cpp
+++ test/test.cpp
@@ -4,34 +4,6 @@
 using namespace std;
 
 int main() {
-    bool flag;
-    int a, b;
-    std::vector<std::vector<int> > repeat;
-    std::cout << "{";
-    for (int i = 1; i <= 6; i++) {
-        for (int j = 1; j <= 6; j++) {
-            for (int k = 1; k <= 6; k++) {
-                a = 0;
-                b = 0;
-                if (i == 1 || i == 4)a += i;
-                else b += i;
-                if (j == 1 || j == 4)a += j;
-                else b += j;
-                if (k == 1 || k == 4)a += k;
-                else b += k;
-                flag = false;
-                for (auto ss: repeat) {
-                    if (a == ss[0] && b == ss[1]) {
-                        flag = true;
-                        break;
-                    }
-                }
-                if (flag)continue;
-                std::cout << "{" << a << "," << b << "},";
-                repeat.push_back({a, b});
-            }
-        }
-    }
-    std::cout << "}";
+    cout << "hello world";
     return 0;
 }
